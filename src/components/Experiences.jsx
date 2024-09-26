c7be9c7e49fb932c52cd7020a42ffb0a7821da8d
Eya <eyabenaziza11@gmail.com>
2024-09-26 20:09:00 +0200

    experience component

---
+++ src/components/Experiences.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Experiences = () => {
+  return (
+    <div>
+   
+   <h2 style={{ color: 'white', margin: '60px', fontSize: '50px' }}>Experiences</h2>
+        <div id="experiences" 
+        style={{ color: 'white', backgroundColor: 'rgb(27, 26, 26)', marginTop: '200px', width: '90%', margin: '60px' }}
+        class="container" >
+            <div className="experience" style={{ margin: '60px' }}>
+                <h2>software developer </h2>
+                <h2>company name: google</h2>
+                <p>Lorem ipsum dolor sit amet consectetur </p>
+                <a href="#" target="_blank">
+                    <button >view experience</button>
+                </a>
+                
+            </div>
+            <div className="experience" style={{ margin: '60px' }}>
+                <h2>software developer</h2>
+                <h2>company name: microsoft</h2>
+                <p>Lorem ipsum dolor sit amet consectetur </p>
+                <a href="#" target="_blank">
+                    <button >view experience</button>
+                </a>
+            </div>
+           
+        </div>
+        
+    </div>
+  )
+}
+
+export default Experiences
