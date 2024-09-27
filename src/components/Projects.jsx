f817877250b04de754273a600ab4bc280571ce55
Eya <eyabenaziza11@gmail.com>
2024-09-27 20:11:37 +0200

    used github api and fetched project's data dynamically

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,28 +1,58 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+
 
 const Projects = () => {
+
+  
+  const [projects,setProjects] = useState([]);
+  
+
+  useEffect(() => {
+    const fetchProjects= async () => {
+      try {
+        const res= await fetch('https://api.github.com/users/Eya-ben-aziza/repos');
+        console.log(res);
+        const data= await res.json();
+        console.log(data);
+        setProjects(data);
+        console.log(projects);
+      }
+      catch (error) {
+        console.log("error fetching data", error);
+      }
+      
+
+    };
+
+    fetchProjects();
+    }
+  ,[])
+
+
   return (
     <div>
       <h2 style={{ color: 'white', margin: '60px', fontSize: '50px' }}>Projects</h2>
         <div id="Projects" style={{color: 'white'}} className="projectContainer">
-            <div className="project">
-                <img src="src/assets/proj1Pic.jpg" alt="proj1 picture" className="projectImage"></img>
-                <h2>project1</h2>
-                <br></br>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex ipsum odit incidunt fugiat nisi praesentium nostrum reprehenderit quam suscipit magnam facilis illum reiciendis qui quasi nihil iusto, provident aperiam amet?</p>
-            </div>
-            <div className="project">
-                <img src="src/assets/proj2Pic.jpg" alt="proj2 picture" className="projectImage"></img>
-                <h2>project2</h2>
-                <br></br>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere incidunt aliquam odio neque hic, sit omnis natus dolorem et magnam cumque ratione quia, laudantium esse sed veritatis, vel eligendi rerum.</p>
-            </div>
-            <div className="project">
-                <img src="src/assets/proj3Pic.jpg" alt="proj3 picture" className="projectImage"></img>
-                <h2>project3</h2>
-                <br></br>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil reiciendis beatae doloribus, similique dolore possimus tenetur maiores, quod aperiam repellendus minus maxime esse suscipit quaerat veritatis velit autem id sunt!.</p>
-            </div>
+            {
+              projects.map( (project) => {
+                console.log(project);
+                return (
+                  <div className="project">
+                  <img src="src/assets/proj1Pic.jpg" alt="proj1 picture" className="projectImage"></img>
+                  <h2>{project.name}</h2>
+                  <br></br>
+                  <p>{project.id}</p>
+                </div>
+                )
+                
+
+              }
+
+              )
+            }
+          
+            
         </div>
     </div>
   )
