07962c40293b7cdec2184554aa01b81f03db3e21
Eya <eyabenaziza11@gmail.com>
2025-02-04 19:59:02 +0100

    changed linear gradient to radial with moving the center to the top

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -3,7 +3,7 @@ import Home from '../components/Home'
 
 const Portfolio = () => {
   return (
-    <div className="min-h-screen bg-gradient-to-r from-[#0f051d] to-[#301a68] text-white relative overflow-hidden">
+    <div className="min-h-screen bg-[radial-gradient(circle_at_50%_-10%,_#301a68_30%,_#0f051d_100%)] text-white relative overflow-hidden">
       <div className="absolute inset-0 bg-[url('/grid.svg')] bg-center opacity-20"></div>
       <Navbar />
       <Home />
