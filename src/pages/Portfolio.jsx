ff894cca79135e43d4cbcd8a0907a4fbfea14ced
Eya <eyabenaziza11@gmail.com>
2025-02-12 17:07:31 +0100

    imported all components into portfolio page

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,14 +1,26 @@
 import Navbar from '../components/Navbar'
 import Home from '../components/Home'
 import About from '../components/About'
+import Experience from '../components/Experience'
+import Skills from '../components/Skills'
+import Projects from '../components/Projects'
+import Education from '../components/Education'
+import Contact from '../components/Contact'
+
 
 const Portfolio = () => {
   return (
-    <div className="min-h-screen bg-[radial-gradient(circle_at_50%_-10%,_#301a68_30%,_#0f051d_100%)] text-white relative overflow-hidden">
+    // <div className="min-h-screen bg-[radial-gradient(circle_at_50%_-10%,_#301a68_30%,_#0f051d_100%)] text-white relative overflow-hidden">
+    <div className="min-h-screen bg-gradient-to-r from-[#0f051d] to-[#301a68] text-white relative overflow-hidden">
       <div className="absolute inset-0 bg-[url('/grid.svg')] bg-center opacity-20"></div>
       <Navbar />
       <Home />
       <About />
+      <Experience />  
+      <Skills />
+      <Projects />
+      <Education />
+      <Contact />
     </div>
   )
 }
