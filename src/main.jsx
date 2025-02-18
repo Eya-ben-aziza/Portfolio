85d8b583e78fb1a130c801bff03ee7a65fad788a
Eya <eyabenaziza11@gmail.com>
2025-02-18 18:33:05 +0100

    fix deployment issues

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css'
+import { HashRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <HashRouter>
     <App />
-  </StrictMode>,
+  </HashRouter>,
 )
